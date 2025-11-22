45e64bd74a22fc4918a5ab6301ba4d474b027386
Osiel De Oliveira <puddlemaster592@gmail.com>
2025-11-22 15:41:50 -0500

    yeah

---
+++ script.js
@@ -0,0 +1,26 @@
+function goToForms() {
+    // 30% chance of redirecting to wrong page
+    if (Math.random() < 0.3) {
+        alert("ERROR 418: I'm a teapot.");
+        window.location.href = "appointments.html";
+    } else {
+        window.location.href = "form.html";
+    }
+}
+
+function goToAppointments() {
+    alert("Processing your request... This may take 6–8 weeks.");
+    window.location.href = "appointments.html";
+}
+
+function randomRedirect() {
+    let pages = ["success.html", "form.html", "index.html", "appointments.html"];
+    let page = pages[Math.floor(Math.random() * pages.length)];
+
+    alert("Redirecting due to calendar malfunction...");
+    window.location.href = page;
+}
+
+function startOver() {
+    window.location.href = "index.html";
+}
